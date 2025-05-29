1279a5ce260f220eb18b54420fc8c7f564356b26
David Sarrat González <113605621+davidsarratgonzalez@users.noreply.github.com>
2025-05-29 18:31:41 +0200

    style: improve cell style

--- install.r
+++
@@ -1,7 +0,0 @@
-install.packages('DSI')
-install.packages('DSOpal')
-
-install.packages('dsBaseClient', repos=c(getOption('repos'), 'http://cran.datashield.org'), dependencies=TRUE)
-
-install.packages("metafor")
-devtools::install_github("timcadman/ds-helper")
